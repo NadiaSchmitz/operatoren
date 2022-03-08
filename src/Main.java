34e8ec4a7813ab5383853d20e5c4d5b95db6c9d2
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-03-08 09:48:01 +0100

    08032022

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,12 @@ public class Main {
 		prozentsatz = 0;
 		
 		if (zinsen > 0) {
-			
 			prozentsatz = zinsen * 100 / kapital;
-			
 		} else {
-			
 			System.out.println("Fehler! Zinsen <= 0.");
-			
 		}
 		
 		System.out.println("Prozentsatz beträgt: " + prozentsatz + "%");
-
 	}
 
 }
